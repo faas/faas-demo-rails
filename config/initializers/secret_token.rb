4789d0a26d21694b3894b12e80cafa02f37e4225
Steven Bull <steven@thebulls.us>
2013-08-31 04:59:59 +0000

    New Rails 4.0 app: `rails new faas_demo -d postgresql -T`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FaasDemo::Application.config.secret_key_base = '3d4fa23dcbd0718d3b01e631c5024fd07d0148c46da46705357e4c40b09e3f038ac30e2159c3bb14fb19d7e24abcd38dbd44ef6ef020268f51e989d970292ac1'
